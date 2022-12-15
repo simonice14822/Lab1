a6e11d9c1405f39392012d85ae5ea5692527ba7a
VA1ENTiN0 <95857891+simonice14822@users.noreply.github.com>
2022-12-16 02:27:40 +0700

    Add files via upload

---
+++ Lab 1 62543502041-1/src/HelloWorldProject/main.java
@@ -0,0 +1,15 @@
+package HelloWorldProject;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println( "Hello World" ) ;
+		// TODO Auto-generated method stub
+
+	}
+
+}
